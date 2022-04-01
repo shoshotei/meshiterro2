a9cc591c6ba9ba3ce28ece0b61ffcd0f41eb8f91
EC2 Default User <ec2-user@ip-172-31-28-133.ec2.internal>
2022-04-01 12:14:25 +0000

    [Add]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root to:"homes#top"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to:"homes#top"
+
+  resources :post_images, only: [:new, :create, :index, :show]
+
   get '/homes/about' => 'homes#about', as: 'about'
   #↑名前付きルートにする場合、［as: '変更したい名前'］を後ろに記述する。
 end
